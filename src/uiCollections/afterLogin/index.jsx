f6ae03e6ae8ef610ff47db4c0bf0ebac6d17913e
vinayk0523 <vkpaul2001@gmail.com>
2024-02-14 00:52:26 +0530

    changes in afterLogin

--- src/uiCollections/afterLogin/index.jsx
+++ src/uiCollections/afterLogin/index.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { json, useLocation, useNavigate } from 'react-router-dom';
 import '../../media/css/style.css';
 import { useEffect } from 'react'; 
 import NavbarIndex from '../navbar';
 import axios from 'axios';
-// import { Link } from 'react-router-dom';
+
 
 
 const AfterLogin = () => {
@@ -13,7 +13,7 @@ const AfterLogin = () => {
   const navigate = useNavigate();
 
   const [toast, showToast] = useState(false);
-  // const [showDialog, setShowDialog] = useState(false); 
+  
 
   useEffect(() => {
     showToast(true);
@@ -23,64 +23,42 @@ const AfterLogin = () => {
   }, []);
 
   const getData= async()=>{
+
+
     console.log("inside getdata()");
     const res= await axios.get('http://localhost:5000/userr/getdata' ,{withCredentials: true });
     console.log("tesing---", res);
     console.log("after getting response");
-    const {id, name}= res.data.authData;  
-    console.log(id,name);
+    const jsdata = JSON.stringify(res.data);
+    // const {id, name}= res.data.authData;  
+    // console.log(id,name);
 
-    if(res.data.message === "session expired"){
-      document.getElementById('displayData').innerText = `${res.data.message}`;
+    if(res.data === 'user logged out'){
+      // document.getElementById('displayData').innerText = `${res.data.message}`;
+      console.log("eror")
       navigate("/");
     }else{
-      document.getElementById('displayData').innerText = `id: ${id}, name: ${name}`;
+      // document.getElementById('displayData').innerText = `id: ${id}, name: ${name}`;
+      document.getElementById('displayData').innerText = `${jsdata}`;
+      // document.getElementById('displayData').innerText = "user exist yoo!!";
     }
   };
 
   
-  // const handleImageClick = () => {
-  //   // setShowDialog(true);
-  //     setShowDialog(prevState => !prevState);
-    
-  // };
 
-  
-  // const DialogBox = () => {
-  //   return (
-  //     <div className="dialogBox">
-  //       <ul className='uList'>
-  //         <li className='lItem' >Home</li>
-
-  //         <li className='lItem' >Profile Section</li>
-  
-        
-  //         <li className='lItem' ><Link to='/policy' className='linkTo'>Policy Docs</Link></li>
-  //         <li className='lItem' >Attendance</li>
-
-  //       </ul>
-  //     </div>
-  //   );
-  // };
 
   return (
     <>
-    
-      {/* <img
-        src="https://e7.pngegg.com/pngimages/124/176/png-clipart-hamburger-button-computer-icons-menu-minimalist-menu-angle-text.png"   
-        alt="Logo"
-        className="logoImage"
-        onClick={handleImageClick} 
-      />
-
-      {showDialog && <DialogBox />} */}
 
       <div className="afterloginContainer">
         <div className="messageBox">
           Hi My Name is {displayName}. I am currently SDE Intern at Investwell
         </div>
         <button className="switchButton" onClick={getData}>Check Session</button>
+        <div id="displayData"></div>
       </div>
+
+      
       <NavbarIndex/>
     
       {toast && <div className="toast">Login Successful</div>}
